46d42390ccc19f2c38b28ee4581676dd2b4eeda7
Crowdbotics <team@crowdbotics.com>
2021-08-14 19:51:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "chippers_29811Identifier",
+  url: "https://chippers_29811Identifier.botics.co",
+  api: "https://chippers_29811Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
